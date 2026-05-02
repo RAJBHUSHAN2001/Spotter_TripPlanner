d912f110b352ebc2cce2d25bfb4b02c3aa62738a
RAJ <2001rajbhushan2001@gmail.com>
2026-05-03 00:48:16 +0530

    Fix CORS configuration - specify allowed origins

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -136,7 +136,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://spotter-trip-planner.vercel.app',
+    'http://localhost:5173',
+    'http://127.0.0.1:5173'
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
